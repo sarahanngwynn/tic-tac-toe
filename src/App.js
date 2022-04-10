00f23fafe98c7b66d0d650eb9a9943c66ceef1de
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2022-04-10 12:06:38 -0500

    added player, and onClick created change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board.js";
 import "./global.css";
 
 export default function App() {
+  const [currentPlayer, setNextPlayer] = useState({ player: 1, letter: "X" });
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Tic-Tac-Toe</h1>
-      <Board />
+      <p>
+        {" "}
+        Player: {currentPlayer.player} {currentPlayer.letter}
+      </p>
+      <Board currentPlayer={currentPlayer} setNextPlayer={setNextPlayer} />
     </div>
   );
 }
